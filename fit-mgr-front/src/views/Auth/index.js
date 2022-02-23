9d9f30179b77e8f73cf685ca9ee551243f06a80e
hairyOwl <lxy_happy@126.com>
2022-02-23 15:56:12 +0800

    认证路由

---
+++ fit-mgr-front/src/views/Auth/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Description: 认证页面逻辑
+ * @Author: hairyOwl
+ * @Date: 2022-02-23 15:41:27
+ * @LastEditors: hairyOwl
+ * @LastEditTime: 2022-02-23 15:47:11
+ */
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    //只在初始化的时候运行
+    setup(){
+
+    },
+});
---
+++ fit-mgr-front/src/views/Auth/index.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Description: 认证页面模板
+ * @Author: hairyOwl
+ * @Date: 2022-02-23 15:41:19
+ * @LastEditors: hairyOwl
+ * @LastEditTime: 2022-02-23 15:44:24
+-->
+<template>
+    <div>
+        auth
+    </div>
+</template>
+
+<script src = "./index.js" ></script>
+
+<style lang="scss">
+    @import './index.scss';
+</style>
